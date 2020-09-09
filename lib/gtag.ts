f1c98cd9d2e8584f0a6aab98b552a6d35e9dcaf8
hyroky867 <handm755@gmail.com>
2020-09-10 00:20:28 +0900

    feat: gtagのイベント関数を実装

---
+++ lib/gtag.ts
@@ -0,0 +1,32 @@
+/* eslint-disable @typescript-eslint/camelcase */
+
+export const GOOGLE_ANALYTICS_ID = process.env.GOOGLE_ANALYTICS_ID || '';
+
+export const existsGaId = GOOGLE_ANALYTICS_ID !== '';
+
+// https://developers.google.com/analytics/devguides/collection/gtagjs/pages
+export const pageView = (url: string): void => {
+  if (GOOGLE_ANALYTICS_ID === '') {
+    return;
+  }
+
+  return window.gtag('config', GOOGLE_ANALYTICS_ID, {
+    page_path: url,
+  });
+};
+
+// https://developers.google.com/analytics/devguides/collection/gtagjs/events
+type EventProps = {
+  action: Gtag.EventNames;
+  category: string;
+  label: string;
+  value: number;
+};
+
+export const event = ({ action, category, label, value }: EventProps): void => {
+  return window.gtag('event', action, {
+    event_category: category,
+    event_label: label,
+    value: value,
+  });
+};
