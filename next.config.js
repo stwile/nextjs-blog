f7f596fcfb2aab23eaed18111b8db4b9b967318c
hyroky867 <handm755@gmail.com>
2020-09-15 21:18:08 +0900

    feat: Sentryと連携

--- next.config.js
+++ next.config.js
@@ -1,4 +1,56 @@
-module.exports = {
+const withSourceMaps = require('@zeit/next-source-maps')()
+
+const SentryWebpackPlugin = require('@sentry/webpack-plugin')
+const {
+  NEXT_PUBLIC_SENTRY_DSN: SENTRY_DSN,
+  SENTRY_ORG,
+  SENTRY_PROJECT,
+  SENTRY_AUTH_TOKEN,
+  NODE_ENV,
+  VERCEL_GITHUB_COMMIT_SHA,
+  VERCEL_GITLAB_COMMIT_SHA,
+  VERCEL_BITBUCKET_COMMIT_SHA,
+} = process.env
+
+const COMMIT_SHA =
+  VERCEL_GITHUB_COMMIT_SHA ||
+  VERCEL_GITLAB_COMMIT_SHA ||
+  VERCEL_BITBUCKET_COMMIT_SHA
+
+process.env.SENTRY_DSN = SENTRY_DSN
+const basePath = ''
+
+module.exports = withSourceMaps({
+  serverRuntimeConfig: {
+    rootDir: __dirname,
+  },
+  webpack: (config, options) => {
+    if (!options.isServer) {
+      config.resolve.alias['@sentry/node'] = '@sentry/browser'
+    }
+
+    if (
+      SENTRY_DSN &&
+      SENTRY_ORG &&
+      SENTRY_PROJECT &&
+      SENTRY_AUTH_TOKEN &&
+      COMMIT_SHA &&
+      NODE_ENV === 'production'
+    ) {
+      config.plugins.push(
+        new SentryWebpackPlugin({
+          include: '.next',
+          ignore: ['node_modules'],
+          stripPrefix: ['webpack://_N_E/'],
+          urlPrefix: `~${basePath}/_next`,
+          release: COMMIT_SHA,
+        })
+      )
+    }
+    return config
+  },
+  basePath,
+
   async redirects() {
     return [
       {
@@ -8,4 +60,4 @@ module.exports = {
       },
     ]
   },
-}
+})
--- pages/404.tsx
+++ pages/404.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
+import Error from 'next/error';
 
 const Custom404: React.FC = () => {
-  return <h1>404 - Page Not Found</h1>;
+  return <Error statusCode={404} />;
 };
 
 export default Custom404;
