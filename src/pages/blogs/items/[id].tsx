950300eb4a6cf4abaef7708a36b768f6c5387893
hyroky867 <handm755@gmail.com>
2021-05-21 00:23:04 +0900

    fix: separete api

--- src/pages/blogs/items/[id].tsx
+++ src/pages/blogs/items/[id].tsx
@@ -1,9 +1,10 @@
-import { GetStaticPaths } from 'next';
+import { GetStaticPaths, GetStaticProps } from 'next';
 import React from 'react';
 
 import Item from '../../../components/blogs/Item';
 import { ContentType } from '../../../types/response/blog/ContentType';
 import { ListType } from '../../../types/response/blog/ListType';
+import { getContentById } from '../../../apis/blog';
 
 type Props = {
   content: ContentType;
@@ -33,25 +34,17 @@ export const getStaticPaths: GetStaticPaths = async () => {
   };
 };
 
-// eslint-disable-next-line @typescript-eslint/explicit-function-return-type
-export const getStaticProps = async (context: {
-  params: {
-    id: string;
-  };
-}) => {
-  const id = context.params.id;
-  const key: string = process.env.API_KEY as string;
+type Params = {
+  id: string;
+};
 
-  const header = {
-    headers: {
-      'X-API-KEY': key,
-    },
-  };
-
-  const url = `${process.env.ENDPOINT}/blog/${id}`;
-  const res = await fetch(url, header);
-  const content: ContentType = await res.json();
-
+export const getStaticProps: GetStaticProps<Props, Params> = async ({
+  params,
+}): Promise<{ props: Props }> => {
+  if (params === undefined) {
+    throw new Error();
+  }
+  const content = await getContentById(params.id);
   return {
     props: {
       content,
