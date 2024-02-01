b5a6cdc52b8e4742ca2ea74bf0a22f3906c4353b
hyroky867 <handm755@gmail.com>
2024-02-01 21:41:07 +0900

    chore: add Gtag

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,35 +1,16 @@
+import { GoogleTagManager } from '@next/third-parties/google';
 import Document, { Html, Head, Main, NextScript } from 'next/document';
-
-import { GOOGLE_ANALYTICS_ID, existsGaId } from '../lib/gtag';
 
 import type { ReactElement } from 'react';
 
 export default class MyDocument extends Document {
   render(): ReactElement {
+    const GOOGLE_ANALYTICS_ID = process.env.GOOGLE_ANALYTICS_ID;
     return (
       <Html lang="ja">
         <Head>
           {/* Global Site Tag (gtag.js) - Google Analytics */}
-          {existsGaId ? (
-            <>
-              <script
-                defer
-                src={`https://www.googletagmanager.com/gtag/js?id=${GOOGLE_ANALYTICS_ID}`}
-              />
-              <script
-                // eslint-disable-next-line react/no-danger
-                dangerouslySetInnerHTML={{
-                  __html: `
-                  window.dataLayer = window.dataLayer || [];
-                  function gtag(){dataLayer.push(arguments);}
-                  gtag('js', new Date());
-                  gtag('config', '${GOOGLE_ANALYTICS_ID}', {
-                    page_path: window.location.pathname,
-                  });`,
-                }}
-              />
-            </>
-          ) : null}
+          {GOOGLE_ANALYTICS_ID && <GoogleTagManager gtmId={GOOGLE_ANALYTICS_ID} />}
         </Head>
         <body>
           <Main />
