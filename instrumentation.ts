075e05fc16a9a93870e7efc1a28fa14523ff7654
hyroky867 <handm755@gmail.com>
2024-09-14 15:07:52 +0900

    chore: update sentry package

---
+++ instrumentation.ts
@@ -0,0 +1,29 @@
+import * as Sentry from '@sentry/nextjs';
+
+export const register = () => {
+  if (process.env.NEXT_RUNTIME === 'nodejs') {
+    // this is your Sentry.init call from `sentry.server.config.js|ts`
+    Sentry.init({
+      dsn: process.env.NEXT_PUBLIC_SENTRY_DSN,
+
+      // Adjust this value in production, or use tracesSampler for greater control
+      tracesSampleRate: 1,
+
+      // Setting this option to true will print useful information to the console while you're setting up Sentry.
+      debug: false,
+    });
+  }
+
+  // This is your Sentry.init call from `sentry.edge.config.js|ts`
+  if (process.env.NEXT_RUNTIME === 'edge') {
+    Sentry.init({
+      dsn: process.env.NEXT_PUBLIC_SENTRY_DSN,
+
+      // Adjust this value in production, or use tracesSampler for greater control
+      tracesSampleRate: 1,
+
+      // Setting this option to true will print useful information to the console while you're setting up Sentry.
+      debug: false,
+    });
+  }
+};
