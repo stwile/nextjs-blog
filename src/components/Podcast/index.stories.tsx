55cb67711de6cfe45ca943b05ea76c3331542cf8
stwile <hyroky916@gmail.com>
2025-03-15 13:26:08 +0900

    feat: ポッドキャストコンポーネントを追加し、ブログページに統合

---
+++ src/components/Podcast/index.stories.tsx
@@ -0,0 +1,18 @@
+import { Podcast } from '.';
+
+import type { Meta, StoryObj } from '@storybook/react';
+
+const meta = {
+  title: 'Podcast',
+  component: Podcast,
+} satisfies Meta<typeof Podcast>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  args: {
+    podcastId: process.env.NEXT_PUBLIC_SB_PODCAST_ID ?? '',
+  },
+  tags: ['skip-test'],
+};
