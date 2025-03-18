7168a3b9cc229a182ad917dd51da3ae0dcba96b0
stwile <hyroky916@gmail.com>
2025-03-19 08:50:14 +0900

    feat: ヘッダーとレイアウトの最大幅を5xlから3xlに変更

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -4,7 +4,7 @@ import { ThemeSwitch } from '../ThemeSwitch';
 import type { JSX } from 'react';
 
 export const Header = (): JSX.Element => (
-  <header className="mx-auto flex max-w-5xl items-center justify-between px-8 py-6">
+  <header className="mx-auto flex max-w-3xl items-center justify-between px-8 py-6">
     <nav aria-label="ヘッダーのナビゲーション">
       <ul className="flex items-center justify-between" aria-label="ヘッダーのリスト">
         <li className="py-4 pr-6">
