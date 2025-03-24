fa2a8edf0c166c4bef5b9679b8c77091416b89aa
stwile <hyroky916@gmail.com>
2025-03-24 18:09:49 +0900

    feat: Sentryの設定とエラーモニタリング用のAPIルートを追加

--- src/pages/_error.tsx
+++ src/pages/_error.tsx
@@ -1,43 +1,17 @@
-// From Next.JS example https://github.com/vercel/next.js/blob/canary/examples/with-sentry-simple/pages/_error.js
 import * as Sentry from '@sentry/nextjs';
-import NextErrorComponent from 'next/error';
+import Error from 'next/error';
 
-import type { NextPageContext } from 'next';
+import type { NextPage } from 'next';
+import type { ErrorProps } from 'next/error';
 
-type MyErrorContext = NextPageContext & {
-  statusCode: number;
-  hasGetInitialPropsRun: boolean;
+const CustomErrorComponent: NextPage<ErrorProps> = ({ statusCode }) => {
+  return <Error statusCode={statusCode} />;
 };
-
-const MyError = ({ statusCode, hasGetInitialPropsRun, err }: MyErrorContext) => {
-  if (!hasGetInitialPropsRun && err) {
-    // getInitialProps is not called in case of
-    // https://github.com/vercel/next.js/issues/8592. As a workaround, we pass
-    // err via _app.js so it can be captured
-    Sentry.captureException(err);
-  }
-
-  return <NextErrorComponent statusCode={statusCode} />;
+CustomErrorComponent.getInitialProps = async (contextData) => {
+  // In case this is running in a serverless function, await this in order to give Sentry
+  // time to send the error before the lambda exits
+  await Sentry.captureUnderscoreErrorException(contextData);
+  // This will contain the status code of the response
+  return Error.getInitialProps(contextData);
 };
-
-MyError.getInitialProps = async ({ res, err, asPath }: MyErrorContext) => {
-  const errorInitialProps = (await NextErrorComponent.getInitialProps({
-    res,
-    err,
-  } as MyErrorContext)) as MyErrorContext;
-
-  errorInitialProps.hasGetInitialPropsRun = true;
-
-  if (err) {
-    Sentry.captureException(err);
-    return errorInitialProps;
-  }
-
-  Sentry.captureException(
-    new Error(`_error.js getInitialProps missing data at path: ${asPath?.toString() ?? ''}`),
-  );
-
-  return errorInitialProps;
-};
-
-export default MyError;
+export default CustomErrorComponent;
