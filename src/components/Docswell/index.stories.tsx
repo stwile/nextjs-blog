8a0dd2700f3af995f80e16653a168492699293aa
stwile <hyroky916@gmail.com>
2025-04-14 16:48:48 +0900

    feat: Docswellコンポーネントを追加
    - .env.local.exampleにNEXT_PUBLIC_SB_DOCSWELL_IDを追加
    - src/components/Docswell/index.tsxを新規作成し、Docswellコンポーネントを実装
    - src/components/Docswell/index.stories.tsxを新規作成し、Docswellコンポーネントのストーリーを追加

---
+++ src/components/Docswell/index.stories.tsx
@@ -0,0 +1,18 @@
+import { Docswell } from '.';
+
+import type { Meta, StoryObj } from '@storybook/react';
+
+const meta = {
+  title: 'Docswell',
+  component: Docswell,
+} satisfies Meta<typeof Docswell>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  args: {
+    slideId: process.env.NEXT_PUBLIC_SB_DOCSWELL_ID ?? '',
+  },
+  tags: ['skip-test'],
+};
