f6e8fd92523825370f9c0042e4732d4b10a48cb7
stwile <hyroky916@gmail.com>
2025-04-20 11:28:10 +0900

    feat: Docswellコンポーネントのリファクタリング
    - Scriptのインポートを削除
    - iframeを使用してスライドを埋め込む実装に変更
    - スタイルをコンテナとiframeに適用

--- src/components/Docswell/index.tsx
+++ src/components/Docswell/index.tsx
@@ -1,5 +1,3 @@
-import Script from 'next/script';
-
 import type { FC } from 'react';
 
 type Props = {
@@ -7,18 +5,35 @@ type Props = {
 };
 
 export const Docswell: FC<Props> = ({ slideId }) => {
-  const SLIDE_CLASS_NAME = 'docswell-embed';
-  const URL = 'https://www.docswell.com';
+  const slideUrl = `https://www.docswell.com/slide/${slideId}/embed`;
+
+  const containerStyle = {
+    display: 'block',
+    width: '100%',
+    margin: '1.5rem 0',
+    paddingBottom: 'calc(56.25% + 38px)',
+    position: 'relative' as const,
+    height: 0,
+  } as const;
+
+  const iframeStyle = {
+    position: 'absolute' as const,
+    top: 0,
+    left: 0,
+    width: '100%',
+    height: '100%',
+    border: 'none',
+  } as const;
+
   return (
-    <>
-      <Script
-        async
-        className={SLIDE_CLASS_NAME}
-        src={`${URL}/assets/libs/docswell-embed/docswell-embed.min.js`}
-        data-src={`${URL}/slide/${slideId}/embed`}
-        data-aspect="0.5625"
-        strategy="lazyOnload"
+    <span style={containerStyle}>
+      <iframe
+        title="docswell"
+        src={slideUrl}
+        allowFullScreen={true}
+        style={iframeStyle}
+        loading="lazy"
       />
-    </>
+    </span>
   );
 };
